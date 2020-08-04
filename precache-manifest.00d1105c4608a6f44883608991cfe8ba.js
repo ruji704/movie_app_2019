22e3680c7b6c7d0c176bcf0c76cbb564b2bbee07
ruji704 <ruji704@gmail.com>
2020-08-04 21:11:52 +0900

    Updates

---
+++ precache-manifest.00d1105c4608a6f44883608991cfe8ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82259df96854b64c2c8e82fba623140f",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "01057318d91d6c1c14b8",
+    "url": "/movie_app_2019/static/css/main.d9de3c2f.chunk.css"
+  },
+  {
+    "revision": "b465fb3a41db387c5800",
+    "url": "/movie_app_2019/static/js/2.6760420d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.6760420d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01057318d91d6c1c14b8",
+    "url": "/movie_app_2019/static/js/main.1ec285f3.chunk.js"
+  },
+  {
+    "revision": "10a8e46fe0d1ddf64ca9",
+    "url": "/movie_app_2019/static/js/runtime-main.15105636.js"
+  }
+]);
